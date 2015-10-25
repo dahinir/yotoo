7780ce22879a652f9a818bd99b8c43fd977313a3
Daehyeon Shin <dahini@gmail.com>
2015-10-25 18:23:51 +0900

    clear

--- app/controllers/allowPushAlertDialog.js
+++ app/controllers/allowPushAlertDialog.js
@@ -1,42 +1,49 @@
 var options;
-var pushRegHandlers = {
-	callback: function(e){
-		// alert("data: "+JSON.stringify(e.data) +"\n"+e.inBackground);
-		// 뱃지를 0으로 하는거를 무시하지 않으면 무한 반복 푸쉬 됨..
-		if(  e.data.badge === 0 ){
-			return;
-		}
-		AG.notifyController.push({
-			pushEvent: e
-		});
-	},
-	error: function(e){
-		Ti.API.debug("[allowPushAlertDialog] register for pushnotification error");
-		_.isFunction(options.error) && options.error();
-	},
-	success: function(e){
-		AG.setting.save("deviceToken", e.deviceToken || Ti.Network.getRemoteDeviceUUID()  );
-		// subscribePushChannel(options);
-		_.isFunction(options.success) && options.success();
-	}
-};
+
 
 function onClick(e){
   // Ti.API.info('e.cancel: ' + e.cancel);
   // Ti.API.info('e.source.cancel: ' + e.source.cancel);
   // Ti.API.info('e.index: ' + e.index);
   if (e.index == e.source.cancel){
-    Ti.API.info('The cancel button was clicked');
+    Ti.API.info("[allowPushAlertDialog] The cancel button was clicked.");
   }else{
+		var pushRegHandlers = {
+			callback: function(e){
+				// alert("data: "+JSON.stringify(e.data) +"\n"+e.inBackground);
+				// 뱃지를 0으로 하는거를 무시하지 않으면 무한 반복 푸쉬 됨..
+				if(  e.data.badge === 0 ){
+					return;
+				}
+				AG.notifyController.push({
+					pushEvent: e
+				});
+			},
+			error: function(e){
+				Ti.API.warn("[allowPushAlertDialog] register for pushnotification error");
+				_.isFunction(options.error) && options.error();
+			},
+			success: function(e){
+				Ti.API.debug("[allowPushAlertDialog] register for pushnotification success");
+				AG.setting.save("deviceToken", e.deviceToken || Ti.Network.getRemoteDeviceUUID()  );
+				// subscribePushChannel(options);
+				_.isFunction(options.success) && options.success();
+			}
+		};
     if(AG.platform.osVersion >= 8){
+			/*
       function register() {
         Ti.API.debug("[allowPushAlertDialog] usernotificationsettings event!! categories:" + e.categories);
         Ti.Network.registerForPushNotifications(pushRegHandlers);
         // Remove event listener once registered for push notifications
         Ti.App.iOS.removeEventListener("usernotificationsettings", register);
       }
+			*/
       // Wait for user settings to be registered before registering for push notifications
-      Ti.App.iOS.addEventListener("usernotificationsettings", register);
+      Ti.App.iOS.addEventListener("usernotificationsettings", function(){
+				Ti.API.debug("[allowPushAlertDialog] usernotificationsettings event!! categories:" + e.categories);
+        Ti.Network.registerForPushNotifications(pushRegHandlers);
+			});
       // Register notification types to use. this will fire "usernotificationsettings" event
       Ti.App.iOS.registerUserNotificationSettings({
         types: [Ti.App.iOS.USER_NOTIFICATION_TYPE_ALERT,Ti.App.iOS.USER_NOTIFICATION_TYPE_SOUND, Ti.App.iOS.USER_NOTIFICATION_TYPE_BADGE]
